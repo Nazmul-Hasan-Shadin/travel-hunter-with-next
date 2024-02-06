af79882ae8a2a6f53d515222c1947a9b7ec3cff0
Nazmul-Hasan-Shadin <nazmulhasanshadin000@gmail.com>
2024-02-06 13:36:57 +0600

    register form validate has added

--- src/app/(auth)/register/page.js
+++ src/app/(auth)/register/page.js
@@ -1,13 +1,27 @@
-import React from "react";
+"use client";
+import { useForm } from "react-hook-form";
 import loginImage from "@/assets/login/log.png";
 import Image from "next/image";
 const register = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  console.log(errors);
   return (
     <div className="flex flex-col-reverse lg:flex-row">
       <div className="lg:flex-1 bg-[#000000] pt-5">
         <div className="hero min-h-screen ">
           <div className="card shrink-0 w-full max-w-sm shadow-2xl ">
-            <form className="card-body p-2 md:p-4">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="card-body p-2 md:p-4"
+            >
               <div className="mt-8">
                 <h2 className=" text-xl lg:text-2xl text-white">Register</h2>
                 <p className="text-sm md:text-md text-gray-400 my-2">
@@ -18,11 +32,17 @@ const register = () => {
               <div className="form-control">
                 <label className="label text-white">Your Name</label>
                 <input
-                  type="email"
+                  {...register("name", {
+                    required: "Name Field is required",
+                  })}
+                  type="text"
                   placeholder="Your Name"
                   className="input input-bordered  border-b-4 border-b-[#008080]"
-                  required
                 />
+
+                <span className="text-red-600">
+                  {errors?.name?.message && `${errors.name.message}`}
+                </span>
               </div>
 
               <div className="form-control">
@@ -33,19 +53,30 @@ const register = () => {
                   type="email"
                   placeholder="email"
                   className="input input-bordered  border-b-4 border-b-[#008080]"
-                  required
+                  {...register("email", {
+                    required: "Email field is required",
+                  })}
                 />
+               <span className="text-red-400">
+               {errors?.email?.message && `${errors.email.message}`}
+               </span>
               </div>
               <div className="form-control">
                 <label className="label">
                   <span className="label-text text-white">Password</span>
                 </label>
                 <input
+                  {...register("password", {
+                    required: "Password field is required",
+                  })}
                   type="password"
                   placeholder="password"
                   className="input input-bordered border-b-4 border-b-[#008080]"
-                  required
                 />
+                <span className="text-red-500">
+                {errors?.password?.message && `${errors.password.message}`}
+
+                </span>
               </div>
 
               <div className="form-control">
@@ -55,11 +86,18 @@ const register = () => {
                   </span>
                 </label>
                 <input
+                 {...register("password", {
+                  required: "Password field is required",
+                })}
                   type="password"
                   placeholder="password"
                   className="input input-bordered border-b-4 border-b-[#008080]"
                   required
                 />
+                <span className="text-red-400">
+                {errors?.password?.message && `${errors.password.message}`}
+                
+                </span>
               </div>
 
               <div className="form-control mt-6">
